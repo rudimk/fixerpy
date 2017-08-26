69583b5b30f9420fc9839e63d95ce45d76272fcd
Rudi MK <rudraksh.mk@icloud.com>
2017-08-26 14:40:31 +0530

    Modified the getLatestRates function to accept a baseCurrency parameter, to allow switching to a base currency other than EUR.

--- fixerpy/lib.py
+++ fixerpy/lib.py
@@ -9,8 +9,12 @@ class Converter:
     targetCurrency = None
     queryDate = None
 
-    def getLatestRates(self):
-        currentRates = r.get('http://api.fixer.io/latest').json()
+    def getLatestRates(self, baseCurrency=None):
+        if baseCurrency:
+            self.baseCurrency = baseCurrency
+            currentRates = r.get('http://api.fixer.io/latest?base=' + baseCurrency).json()
+        else:
+            currentRates = r.get('http://api.fixer.io/latest').json()
         return currentRates
 
 
